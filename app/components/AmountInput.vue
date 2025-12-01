34679880302e3240accef918f4e01e3238089a91
Iwuchukwu Divine Ifunanya <ekeneifunanya@gmail.com>
2025-12-01 04:56:31 +0100

    feat: enhance component layouts and improve user interactions
    - Refactored AmountInput.vue, SelectBank.vue, SelectWallet.vue, and other components for better layout and responsiveness.
    - Updated styling and structure in BottomSlider.vue and ServiceInput.vue to enhance user experience.
    - Improved modal handling in electricity.vue and added LazyBottomSlider for better bank selection.
    - Standardized code formatting and organization across various components for consistency.

--- app/components/AmountInput.vue
+++ app/components/AmountInput.vue
@@ -1,67 +1,77 @@
 <template>
-    <div :class="orientation">
-        <div class="flex items-center justify-center gap-2">
-           <span class="text-text-black text-3xl font-bold">&#8358;</span>
-           <input
-               :modelValue="modelValue"
-               @input="handleInput"
-               inputmode="numeric"
-               :placeholder="placeholder"
-               class="text-[20px] text-center"
-               maxlength="10"
-               pattern="[0-9]*"
-           />
-        </div>
-        <app-button class="btn" @click="onClick" :disabled="disabled" :title="buttonTitle" />
+  <div :class="orientation">
+    <div class="flex items-center">
+      <span class="text-text-black block text-3xl font-bold">&#8358;</span>
+      <input
+        :modelValue="modelValue"
+        @input="handleInput"
+        inputmode="numeric"
+        :placeholder="placeholder"
+        class="text-[20px] w-full text-center"
+        maxlength="10"
+        pattern="[0-9]*"
+      />
     </div>
+    <app-button
+      class="btn"
+      @click="onClick"
+      :disabled="disabled"
+      :title="buttonTitle"
+    />
+  </div>
 </template>
 
 <script lang="ts" setup>
-const emit = defineEmits(['update:modelValue'])
-const props = withDefaults(defineProps<{
-  modelValue: string | number
-  placeholder?: string
-  orientation?: 'vertical' | 'horizontal'
-  disabled?: boolean
-  buttonTitle: string
-  onClick?: () => void
-}>(), {
-  modelValue: 0,
-  placeholder: 'Enter Amount',
-  orientation: 'vertical',
-  disabled: false,
-})
+const emit = defineEmits(["update:modelValue"]);
+const props = withDefaults(
+  defineProps<{
+    modelValue: string | number;
+    placeholder?: string;
+    orientation?: "vertical" | "horizontal";
+    disabled?: boolean;
+    buttonTitle: string;
+    onClick?: () => void;
+  }>(),
+  {
+    modelValue: 0,
+    placeholder: "Enter Amount",
+    orientation: "vertical",
+    disabled: false,
+  }
+);
 
 const handleInput = (event: Event) => {
-  const input = event.target as HTMLInputElement
-  const numericValue = input.value.replace(/[^0-9]/g, '')
-  input.value = numericValue
-  emit('update:modelValue', numericValue)
-}
+  const input = event.target as HTMLInputElement;
+  const numericValue = input.value.replace(/[^0-9]/g, "");
+  input.value = numericValue;
+  emit("update:modelValue", numericValue);
+};
 </script>
 
-<style  scoped>
-.horizontal{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    gap: 10px;
+<style scoped>
+.horizontal {
+  width: 100% !important;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
 }
-.vertical{
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    gap: 32px;
+.vertical {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 32px;
 }
-.horizontal .btn{
-    max-height: 35px;
+.horizontal .btn {
+  max-height: 35px;
+  width: fit-content;
+  min-width: 86px;
 }
-input{
+input {
+  color: var(--color-text-black);
+  &::placeholder {
     color: var(--color-text-black);
-    &::placeholder{
-        color: var(--color-text-black);
-        opacity: 0.66;
-    }
+    opacity: 0.66;
   }
+}
 </style>
--- app/store/app.ts
+++ app/store/app.ts
@@ -1,16 +1,18 @@
-import { defineStore } from 'pinia';
-import type { Toast } from '~/utils/types/types';
+import { defineStore } from "pinia";
+import { walletOptions } from "~/utils/constants/appData";
+import type { Toast } from "~/utils/types/types";
 
-export const useAppStore = defineStore('app', () => {
-
-  const selectedWallet = ref('')
-  const toasts = ref<Toast[]>([])
-  const removeToast = (id:string) => {
-    toasts.value = toasts.value.filter(toast => toast.id !== id)
-}
+export const useAppStore = defineStore("app", () => {
+  const selectedWallet = ref<(typeof walletOptions)[number] | undefined>(
+    walletOptions[0]
+  );
+  const toasts = ref<Toast[]>([]);
+  const removeToast = (id: string) => {
+    toasts.value = toasts.value.filter((toast) => toast.id !== id);
+  };
   return {
     selectedWallet,
     toasts,
     removeToast,
-  }
+  };
 });
